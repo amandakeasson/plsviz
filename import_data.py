720da366353bd90f4e1b830902428f7280864ca5
balmungsama <jbryaneusebio@gmail.com>
2018-05-04 12:09:33 -0400

    added masking of functional data

--- import_data.py
+++ import_data.py
@@ -4,18 +4,36 @@ import pandas as pd
 import numpy as np
 import nibabel as nib
 
+## Check for mask
+# sample_mask = '/Users/steveneusebio/Documents/brain_hack/pls_m2py/sample_datasets/bin_fun_MNI152.nii.gz'
+# mask_path   = sample_mask
+
+try:
+  mask      = nib.load(mask_path)
+  mask      = mask.get_data()
+
+  mask_dims = mask.shape
+
+  mask_vec  = reduce( (lambda x, y: x * y), mask_dims)
+  mask_vec  = np.reshape(mask, (1,mask_vec))
+  mask_vec  = (mask_vec > 0.5).astype(np.int_)
+
+  st_coords = np.where(mask_vec == 1)[1]
+except:
+  mask = None
+
 ## Import functional data
-# sample = '/Users/steveneusebio/Documents/brain hack/pls_m2py/sample_datasets/sample_list.txt'
+# sample_fun = '/Users/steveneusebio/Documents/brain_hack/pls_m2py/sample_datasets/sample_list.txt'
+# fun_name = sample_fun
+
 # feed in a text file where each line is the absolute path to
 # subject nifti data
-# fun_name = sample
 
 with open(fun_name) as f:
   dsets = [line.rstrip('\n') for line in open(fun_name)]
 
   X_mat = np.array([])
   for dset in dsets:
-    print(dset)
 
     dset = nib.load(dset)
     dset_array = dset.get_data()
@@ -24,6 +42,9 @@ with open(fun_name) as f:
     dset_dims  = dset.shape
     dset_len   = reduce( (lambda x, y: x * y), dset_dims)
     dset_vec   = np.reshape(dset_array, (1,dset_len))
+
+    # apply the mask
+    dset_vec = [dset_vec[0,st_coords]]
 
     try:
       X_mat = np.append(X_mat, dset_vec, 0)
@@ -35,18 +56,21 @@ with open(fun_name) as f:
 #                                 each column = behav measure
 
 # sample data
-# sample = '/Users/steveneusebio/Documents/brain hack/pls_m2py/sample_datasets/behav_sample.csv'
-# behav_nm = sample
+
+# sample_behav = '/Users/steveneusebio/Documents/brain_hack/pls_m2py/sample_datasets/behav_sample.csv'
+# behav_nm = sample_behav
 
 behav = pd.read_csv(behav_nm, sep=',')
 Y_mat = behav.values
 
 ## define groups
-# *optional
+## *optional
+
 # group file must be a csv with one column, 
 # each row defining subject group membership. 
 try:
   G_mat = pd.read_csv(group_file, header=None)
   G_mat = G_mat.set_index([0]).index.values
 except:
-  print('no groups defined')
+  G_mat = [1] * X_mat.shape[0]
+  G_mat = pd.DataFrame(G_mat)
