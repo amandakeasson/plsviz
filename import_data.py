a95a23b5d6c4a8999257658b1dca2a2bd2d0b090
balmungsama <jbryaneusebio@gmail.com>
2018-05-11 13:08:43 -0400

    wrapped in functions, but still need to call functions

--- import_data.py
+++ import_data.py
@@ -8,6 +8,11 @@ import numpy as np
 import nibabel as nib
 from optparse import OptionParser
 
+## sample data 
+# Y_file    = "C:\Users\enter\Documents\Software\gitrepos\plsviz\data_behavior_y.csv"
+# X_file    = "C:\Users\enter\Documents\Software\gitrepos\plsviz\sample_img\x_file_ls.txt"
+# mask_file = "C:\Users\enter\Documents\Software\gitrepos\plsviz\sample_masks\\bin_fun_MNI152.nii.gz"
+
 ## define help text
 help_txt = 'test.py -f/--fun <functional_list.txt> -b/--behav <behav_data.csv> -g/--cond <group_index.txt> -m/--mask <mask.nii>'
 
@@ -18,13 +23,7 @@ parser.add_option("-y",           dest="Y_file"   , type='str', help="Absolute p
 parser.add_option("-m", "--mask", dest="mask_file", type='str', help="Absolute path to the brain/roi mask.")
 (options, args) = parser.parse_args()
 
-## sample data 
-# Y_file    = "C:\Users\enter\Documents\Software\gitrepos\plsviz\data_behavior_y.csv"
-# X_file    = "C:\Users\enter\Documents\Software\gitrepos\plsviz\sample_img\x_file_ls.txt"
-# mask_file = "C:\Users\enter\Documents\Software\gitrepos\plsviz\sample_masks\\bin_fun_MNI152.nii.gz"
-
-
-
+# define variables
 try:
 	options.Y_file
 except:
@@ -36,60 +35,49 @@ X_file    = options.X_file
 Y_file    = options.Y_file
 mask_file = options.mask_file
 
-print(X_file)
-print(Y_file)
-print(mask_file)
-
 ## Import mask
-try:
-	mask      = nib.load(mask_file)
-	mask      = mask.get_data()
-
-	mask_dims = mask.shape
-
-	mask_vec  = reduce( (lambda x, y: x * y), mask_dims)
-	mask_vec  = np.reshape(mask, (1,mask_vec))
-	mask_vec  = (mask_vec > 0.5).astype(np.int_)
-
-	st_coords = np.where(mask_vec == 1)[1]
-except:
-	mask      = None
-	st_coords = None
+def importMask(mask_file):
+	try:
+		mask      = nib.load(mask_file)
+		mask      = mask.get_data()
+		mask_dims = mask.shape
+		mask_vec  = reduce( (lambda x, y: x * y), mask_dims)
+		mask_vec  = np.reshape(mask, (1,mask_vec))
+		mask_vec  = (mask_vec > 0.5).astype(np.int_)
+		st_coords = np.where(mask_vec == 1)[1]
+	except:
+		mask      = None
+		st_coords = None
+	return(mask_dims, st_coords)
 
 ## Import functional data
-dsets = [line.rstrip('\n') for line in open(X_file)]
+def importX(X_file, st_coords):
+	dsets = [line.rstrip('\n') for line in open(X_file)]
+	X_mat = np.array([])
+	for dset in dsets:
+		dset = nib.load(dset)
+		dset_array = dset.get_data()
+		
+		#reshaping dset into vector
+		dset_dims  = dset.shape
+		dset_len   = reduce( (lambda x, y: x * y), dset_dims)
+		dset_vec   = np.reshape(dset_array, (1,dset_len))
 
-X_mat = np.array([])
-for dset in dsets:
+		# apply the mask
+		dset_vec = np.array(dset_vec[0, st_coords])
 
-	dset = nib.load(dset)
-	dset_array = dset.get_data()
-	
-	#reshaping dset into vector
-	dset_dims  = dset.shape
-	dset_len   = reduce( (lambda x, y: x * y), dset_dims)
-	dset_vec   = np.reshape(dset_array, (1,dset_len))
-
-	# apply the mask
-	dset_vec = np.array(dset_vec[0,st_coords])
-
-	try:
-		X_mat = np.append(X_mat, dset_vec, 0)
-	except:
-		X_mat = dset_vec
+		try:
+			X_mat = np.append(X_mat, dset_vec, 0)
+		except:
+			X_mat = dset_vec
+	return(X_mat)
 
 ## import behavioural data
-try:
-	behav = pd.read_csv(Y_file, sep=',')
-	Y_mat = behav.values
-	C_mat = ''
-except:
-	Y_mat = np.eye(X_mat.shape[0])
-
-## define groups
-try:
-	C_mat = pd.read_csv(cond_file, header=None)
-	C_mat = C_mat.set_index([0]).index.values
-except:
-	C_mat = [1] * X_mat.shape[0]
-	C_mat = pd.DataFrame(C_mat)
+def importY(Y_file, X_mat):
+	try:
+		behav = pd.read_csv(Y_file, sep=',')
+		Y_mat = behav.values
+		# TODO you need to add support for group definition
+	except:
+		Y_mat = np.eye(X_mat.shape[0])
+	return(Y_mat)
